cef01015f834925d51e628b6b7f87a6f7cbdad6d
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-18 19:07:34 +0300

    Initial Commit

---
+++ SwiftUISheep2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUISheep2
+//
+//  Created by Артем on 7/18/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
